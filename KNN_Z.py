369327a759915b369df8e12e04208fbd71cde95d
Vtwo <vu.nvv2003@gmail.com>
2024-05-31 16:39:14 +0700

    AI

--- KNN_Z.py
+++ KNN_Z.py
@@ -1,1 +1,77 @@
-{"cells":[{"cell_type":"code","execution_count":4,"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"g8oLAGK6U1aa","executionInfo":{"status":"ok","timestamp":1716864435371,"user_tz":-420,"elapsed":1234765,"user":{"displayName":"K16 NCKH","userId":"11588807329835209005"}},"outputId":"59e71d4c-23fb-4a64-faa0-a1ef38fb86a8"},"outputs":[{"output_type":"stream","name":"stdout","text":["Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True).\n","Kich thuoc du lieu:  (7026, 1016)\n","Dữ liệu thiếu:\n"," -59          0\n","-60          0\n","-62          0\n","-77          0\n","-79          0\n","            ..\n","-100.1005    0\n","-100.1006    0\n","0            0\n","0.1          0\n","13           0\n","Length: 1016, dtype: int64\n","Du lieu vao:        -59  -60  -62  -77  -79  -81  -100  -100.1  -100.2  -100.3  ...  \\\n","0     -57  -61  -60 -100 -100  -85   -69     -76     -82     -83  ...   \n","1     -59  -61  -61  -80  -79  -86   -76     -75    -100    -100  ...   \n","2     -60  -64  -59  -78  -75  -84   -73     -76    -100     -88  ...   \n","3     -59  -62  -59  -77  -76 -100   -71    -100    -100    -100  ...   \n","4     -59  -65  -59  -76 -100 -100   -69     -80    -100     -86  ...   \n","...   ...  ...  ...  ...  ...  ...   ...     ...     ...     ...  ...   \n","7021 -100  -77 -100 -100  -67 -100  -100    -100    -100    -100  ...   \n","7022 -100  -76 -100 -100 -100 -100  -100    -100    -100    -100  ...   \n","7023 -100  -76 -100 -100  -65 -100  -100    -100    -100    -100  ...   \n","7024 -100 -100 -100 -100  -66 -100  -100    -100    -100    -100  ...   \n","7025 -100  -80  -85 -100  -65 -100  -100    -100    -100    -100  ...   \n","\n","      -100.997  -100.998  -100.999  -100.1000  -100.1001  -100.1002  \\\n","0         -100      -100      -100       -100       -100       -100   \n","1         -100      -100      -100       -100       -100       -100   \n","2         -100      -100      -100       -100       -100       -100   \n","3         -100      -100      -100       -100       -100       -100   \n","4         -100      -100      -100       -100       -100       -100   \n","...        ...       ...       ...        ...        ...        ...   \n","7021      -100      -100      -100       -100       -100       -100   \n","7022      -100      -100      -100       -100       -100       -100   \n","7023      -100      -100      -100       -100       -100       -100   \n","7024      -100      -100      -100       -100       -100       -100   \n","7025      -100      -100      -100       -100       -100        -89   \n","\n","      -100.1003  -100.1004  -100.1005  -100.1006  \n","0          -100       -100       -100       -100  \n","1          -100       -100       -100       -100  \n","2          -100       -100       -100       -100  \n","3          -100       -100       -100       -100  \n","4          -100       -100       -100       -100  \n","...         ...        ...        ...        ...  \n","7021       -100       -100       -100       -100  \n","7022       -100       -100       -100       -100  \n","7023       -100       -100       -100       -100  \n","7024       -100       -100       -100       -100  \n","7025       -100       -100       -100       -100  \n","\n","[7026 rows x 1013 columns]\n","Nhan:  0       13\n","1       13\n","2       13\n","3       13\n","4       13\n","        ..\n","7021    14\n","7022    14\n","7023    14\n","7024    14\n","7025    14\n","Name: 13, Length: 7026, dtype: int64\n"]}],"source":["import pandas as pd\n","import numpy as np\n","import time\n","import os\n","from sklearn.model_selection import train_test_split\n","from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score\n","from google.colab import drive\n","\n","class KNN:\n","    def __init__(self, k=3):\n","        self.k = k\n","\n","    def euclidean_distance(self, pointA, pointB):\n","        return np.sqrt(np.sum(np.square(pointB - pointA), axis=1))\n","\n","    def fit(self, X_train, y_train):\n","        self.X_train = X_train\n","        self.y_train = y_train\n","\n","    def predict(self, X_test):\n","        y_pred = []\n","        for test_point in X_test.values:\n","            distances = self.euclidean_distance(self.X_train.values, test_point)\n","            k_nearest_indices = np.argsort(distances)[:self.k]\n","            k_nearest_labels = self.y_train.iloc[k_nearest_indices].values\n","            prediction = np.mean(k_nearest_labels, axis=0)\n","            y_pred.append(prediction)\n","        return np.array(y_pred)\n","\n","\n","    def score(self, X_test, y_test, save_predictions=False, predictions_path=None):\n","        y_pred = np.round(self.predict(X_test))\n","        mse = mean_squared_error(y_test.values, y_pred)\n","        rmse = np.sqrt(mse)\n","        mae = mean_absolute_error(y_test.values, y_pred)\n","        r2 = r2_score(y_test.values, y_pred)\n","        acc = np.mean(np.isclose(y_test.values, y_pred, atol=1))\n","\n","        if save_predictions and predictions_path:\n","            predictions_df = pd.DataFrame(y_pred)\n","            predictions_df.to_csv(predictions_path, index=False)\n","\n","        return mse, rmse, mae, r2, acc, y_pred\n","\n","drive.mount('/content/drive')\n","data = pd.read_csv('/content/drive/MyDrive/NCKH_K16/Tu_3tang_fn.csv')\n","print(\"Kich thuoc du lieu: \", data.shape)\n","\n","# Kiểm tra dữ liệu thiếu\n","missing_data = data.isnull().sum()\n","print(\"Dữ liệu thiếu:\\n\", missing_data)\n","\n","# Xóa các dòng chứa dữ liệu thiếu (nếu có)\n","data = data.dropna()\n","\n","# Lấy đầu vào và nhãn\n","features = data.iloc[:, :-3]\n","print(\"Du lieu vao: \", features)\n","labels = data.iloc[:, -1]\n","print(\"Nhan: \", labels)\n","\n","# Phân chia dữ liệu thành tập huấn luyện và tập kiểm tra\n","X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)\n","results_list = []\n","\n","for k in range(1, 20, 1):\n","    knn = KNN(k=k)\n","    knn.fit(X_train, y_train)\n","\n","    mse, rmse, mae, r2, acc, predictions = knn.score(X_test, y_test, save_predictions=True, predictions_path=f'/content/drive/MyDrive/NCKH_K16/predictions_Tu_knn_z_3T_k_{k}.csv')\n","\n","    result = {'k': k, 'MSE': mse, 'RMSE': rmse, 'MAE' : mae, 'R2' : r2, 'ACC' : acc}\n","    results_list.append(result)\n","\n","# Save the results\n","results_df = pd.DataFrame(results_list)\n","results_df.to_csv('/content/drive/MyDrive/NCKH_K16/results_Tu_knn_z_3T.csv', index=False)"]}],"metadata":{"colab":{"provenance":[]},"kernelspec":{"display_name":"Python 3","name":"python3"},"language_info":{"name":"python"}},"nbformat":4,"nbformat_minor":0}
+import pandas as pd
+import numpy as np
+import time
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
+from google.colab import drive
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def euclidean_distance(self, pointA, pointB):
+        return np.sqrt(np.sum(np.square(pointB - pointA), axis=1))
+
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        y_pred = []
+        for test_point in X_test.values:
+            distances = self.euclidean_distance(self.X_train.values, test_point)
+            k_nearest_indices = np.argsort(distances)[:self.k]
+            k_nearest_labels = self.y_train.iloc[k_nearest_indices].values
+            prediction = np.mean(k_nearest_labels, axis=0)
+            y_pred.append(prediction)
+        return np.array(y_pred)
+
+
+    def score(self, X_test, y_test, save_predictions=False, predictions_path=None):
+        y_pred = np.round(self.predict(X_test))
+        mse = mean_squared_error(y_test.values, y_pred)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test.values, y_pred)
+        r2 = r2_score(y_test.values, y_pred)
+        acc = np.mean(np.isclose(y_test.values, y_pred, atol=1))
+
+        if save_predictions and predictions_path:
+            predictions_df = pd.DataFrame(y_pred)
+            predictions_df.to_csv(predictions_path, index=False)
+
+        return mse, rmse, mae, r2, acc, y_pred
+
+drive.mount('/content/drive')
+data = pd.read_csv('/content/drive/MyDrive/NCKH_K16/Android12.csv')
+print("Kich thuoc du lieu: ", data.shape)
+
+# Kiểm tra dữ liệu thiếu
+missing_data = data.isnull().sum()
+print("Dữ liệu thiếu:\n", missing_data)
+
+# Xóa các dòng chứa dữ liệu thiếu (nếu có)
+data = data.dropna()
+
+# Lấy đầu vào và nhãn
+features = data.iloc[:, :-3]
+print("Du lieu vao: ", features)
+labels = data.iloc[:, -1]
+print("Nhan: ", labels)
+
+# Phân chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+results_list = []
+
+for k in range(1, 20, 1):
+    knn = KNN(k=k)
+    knn.fit(X_train, y_train)
+
+    mse, rmse, mae, r2, acc, predictions = knn.score(X_test, y_test, save_predictions=True, predictions_path=f'/content/drive/MyDrive/NCKH_K16/predictions_knn_z_3T_k_{k}.csv')
+
+    result = {'k': k, 'MSE': mse, 'RMSE': rmse, 'MAE' : mae, 'R2' : r2, 'ACC' : acc}
+    results_list.append(result)
+
+# Save the results
+results_df = pd.DataFrame(results_list)
+results_df.to_csv('/content/drive/MyDrive/NCKH_K16/results_knn_z_3T.csv', index=False)
--- LSTM.py
+++ LSTM.py
@@ -1,1 +1,63 @@
-{"cells":[{"cell_type":"code","execution_count":3,"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"dKUVVk2uL0Q2","executionInfo":{"status":"ok","timestamp":1716871880172,"user_tz":-420,"elapsed":809854,"user":{"displayName":"K16 NCKH","userId":"11588807329835209005"}},"outputId":"469e9582-04df-4d20-eb7e-003b616148ac"},"outputs":[{"output_type":"stream","name":"stdout","text":["Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True).\n","Epoch 1/50\n","1124/1124 [==============================] - 17s 14ms/step - loss: 21.0448\n","Epoch 2/50\n","1124/1124 [==============================] - 17s 15ms/step - loss: 5.8642\n","Epoch 3/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 4.7331\n","Epoch 4/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 3.9716\n","Epoch 5/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 3.5595\n","Epoch 6/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 3.1042\n","Epoch 7/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 2.8185\n","Epoch 8/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 2.5306\n","Epoch 9/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 2.3490\n","Epoch 10/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 2.1743\n","Epoch 11/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 2.0472\n","Epoch 12/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 1.9419\n","Epoch 13/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.8066\n","Epoch 14/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.7434\n","Epoch 15/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.6700\n","Epoch 16/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.5790\n","Epoch 17/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.4856\n","Epoch 18/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.4670\n","Epoch 19/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.3841\n","Epoch 20/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.3636\n","Epoch 21/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 1.3057\n","Epoch 22/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 1.2656\n","Epoch 23/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.2325\n","Epoch 24/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 1.1623\n","Epoch 25/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.1433\n","Epoch 26/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.1178\n","Epoch 27/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 1.0642\n","Epoch 28/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 1.0677\n","Epoch 29/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 1.0442\n","Epoch 30/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9947\n","Epoch 31/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9915\n","Epoch 32/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9613\n","Epoch 33/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9353\n","Epoch 34/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9196\n","Epoch 35/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.9015\n","Epoch 36/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.9066\n","Epoch 37/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 0.8775\n","Epoch 38/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.8423\n","Epoch 39/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.8386\n","Epoch 40/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.8262\n","Epoch 41/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.7876\n","Epoch 42/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.8021\n","Epoch 43/50\n","1124/1124 [==============================] - 16s 15ms/step - loss: 0.7809\n","Epoch 44/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.7456\n","Epoch 45/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.7448\n","Epoch 46/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.7409\n","Epoch 47/50\n","1124/1124 [==============================] - 15s 14ms/step - loss: 0.7517\n","Epoch 48/50\n","1124/1124 [==============================] - 15s 13ms/step - loss: 0.7180\n","Epoch 49/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.7264\n","Epoch 50/50\n","1124/1124 [==============================] - 16s 14ms/step - loss: 0.7213\n"]},{"output_type":"stream","name":"stderr","text":["/usr/local/lib/python3.10/dist-packages/keras/src/engine/training.py:3103: UserWarning: You are saving your model as an HDF5 file via `model.save()`. This file format is considered legacy. We recommend using instead the native Keras format, e.g. `model.save('my_model.keras')`.\n","  saving_api.save_model(\n"]},{"output_type":"stream","name":"stdout","text":["44/44 [==============================] - 1s 4ms/step\n","44/44 [==============================] - 1s 4ms/step - loss: 1.6487\n","Mean Squared Error: 1.6487435102462769\n","Root Mean Square Error: 1.2840340767465157\n","Mean Absolute Error: 0.7053799328785928\n","R^2 Score: 0.9471652291225818\n","Accuracy: 0.009246088193456615\n"]}],"source":["import pandas as pd\n","import numpy as np\n","import time\n","import math\n","from sklearn.model_selection import train_test_split\n","from sklearn.preprocessing import MinMaxScaler\n","from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error\n","from keras.models import Sequential\n","from keras.layers import LSTM, Dropout, Dense\n","from google.colab import drive\n","\n","drive.mount('/content/drive')\n","data = pd.read_csv('/content/drive/MyDrive/NCKH_K16/Tu_3tang_fn.csv')\n","features = data.iloc[:, :-3]\n","labels = data.iloc[:, -3:]\n","\n","# Split data into train and test sets\n","X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)\n","\n","# Normalize the data\n","scaler = MinMaxScaler()\n","X_train = scaler.fit_transform(X_train)\n","X_test = scaler.transform(X_test)\n","\n","# Reshape data to 3D (samples, time steps, features)\n","X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])\n","X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])\n","\n","# Build the LSTM model\n","model = Sequential()\n","model.add(LSTM(240, activation='tanh', input_shape=(X_train.shape[1], X_train.shape[2])))\n","model.add(Dropout(0.3))  # To prevent overfitting\n","model.add(Dense(250, activation='relu'))\n","model.add(Dense(3, activation='linear'))  # Adjusted to output 3 values\n","\n","model.compile(optimizer='adam', loss='mse')\n","\n","# Train the model\n","model.fit(X_train, y_train, epochs=50, batch_size=5, verbose=1)\n","\n","# Save the model\n","model.save('LSTM.h5')\n","\n","# Predict on test set\n","y_pred = model.predict(X_test)\n","\n","# Save predictions to CSV file\n","predictions_df = pd.DataFrame(y_pred, columns=y_test.columns)\n","predictions_df.to_csv('/content/drive/MyDrive/NCKH_K16/predictions_lstm_tu.csv', index=False)\n","\n","# Evaluate the model\n","mse = model.evaluate(X_test, y_test)\n","print('Mean Squared Error:', mse)\n","print('Root Mean Square Error:', math.sqrt(mse))\n","mae = mean_absolute_error(y_test, y_pred)\n","print('Mean Absolute Error:', mae)\n","\n","# Calculate R^2 score\n","print(\"R^2 Score:\", r2_score(y_test, y_pred))\n","\n","threshold = 0.1  # Define a threshold for acceptable error margin\n","accuracy = np.mean(np.all(np.abs(y_test - y_pred) < threshold, axis=1))\n","print('Accuracy:', accuracy)"]}],"metadata":{"colab":{"provenance":[],"mount_file_id":"1iX8i_D0PXwQKF28-A6nRs6tahcgP-Vm2","authorship_tag":"ABX9TyMBWLgIL6fPgGkfAlpLB3/a"},"kernelspec":{"display_name":"Python 3","name":"python3"},"language_info":{"name":"python"}},"nbformat":4,"nbformat_minor":0}
+import pandas as pd
+import numpy as np
+import time
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense
+from google.colab import drive
+
+drive.mount('/content/drive')
+data = pd.read_csv('/content/drive/MyDrive/NCKH_K16/Android12.csv')
+features = data.iloc[:, :-3]
+labels = data.iloc[:, -3:]
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Normalize the data
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Reshape data to 3D (samples, time steps, features)
+X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
+X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(240, activation='tanh', input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.3))  # To prevent overfitting
+model.add(Dense(250, activation='relu'))
+model.add(Dense(3, activation='linear'))  # Adjusted to output 3 values
+
+model.compile(optimizer='adam', loss='mse')
+
+# Train the model
+model.fit(X_train, y_train, epochs=50, batch_size=5, verbose=1)
+
+# Save the model
+model.save('LSTM.h5')
+
+# Predict on test set
+y_pred = model.predict(X_test)
+
+# Save predictions to CSV file
+predictions_df = pd.DataFrame(y_pred, columns=y_test.columns)
+predictions_df.to_csv('/content/drive/MyDrive/NCKH_K16/predictions_lstm.csv', index=False)
+
+# Evaluate the model
+mse = model.evaluate(X_test, y_test)
+print('Mean Squared Error:', mse)
+print('Root Mean Square Error:', math.sqrt(mse))
+mae = mean_absolute_error(y_test, y_pred)
+print('Mean Absolute Error:', mae)
+
+# Calculate R^2 score
+print("R^2 Score:", r2_score(y_test, y_pred))
+
+threshold = 0.1  # Define a threshold for acceptable error margin
+accuracy = np.mean(np.all(np.abs(y_test - y_pred) < threshold, axis=1))
+print('Accuracy:', accuracy)
